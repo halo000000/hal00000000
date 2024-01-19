8d81599ae3cd58b05d76664ff56de2452870a9d2
halo000000 <157131368+halo000000@users.noreply.github.com>
2024-01-19 17:02:53 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,314 @@
+let adus = [{
+name: "al fatiha ",
+src: "audo/001.mp3",
+
+
+},{
+    name: "al baqara ",
+    src: "audo/002.mp3",
+    
+    
+    },{
+  name: "al hmran",
+  src: "audo/003.mp3",
+        
+        
+ },{
+   name: "an nisa ",
+    src: "audo/004.mp3",
+            
+            
+   },{
+    name: "Al-Ma'idah ",
+   src: "audo/005.mp3",
+                
+                
+   },{
+   name: "Al-An'am ",
+    src: "audo/006.mp3",
+  },{
+   name: "Al-A'raf ",
+   src: "audo/007.mp3",
+                        
+                        
+  },{
+    name: "Al-Anfal ",
+    src: "audo/008.mp3",
+                            
+   },{
+   name: "At-Taubah ",
+   src: "audo/009.mp3",
+                                
+                                
+   },{
+   name: " Yunus ",
+   src: "audo/010.mp3",
+                                    
+                                    
+   },{
+  name: " Hood ",
+ src: "audo/011.mp3",
+                                     
+                                     
+ },{
+  name: "Yusuf",
+  src: "audo/012.mp3",
+                                     
+                                       
+      }
+,{ name: " Ar-Ra'd ",
+src: "audo/013.mp3",}
+,{ name: "Ibrahim ",
+src: "audo/014.mp3",}                                    
+,{ name: " Al-Hijr ",
+src: "audo/015.mp3",}                                    
+,{ name: " An-Nahl ",
+src: "audo/016.mp3",}
+,{ name: "Al-Isra ",
+src: "audo/017.mp3",}
+,{ name: " Al-Kahf ",
+src: "audo/018.mp3",}         
+,{ name: " Maryam ",
+src: "audo/019.mp3",}
+,{ name: " Ta­Ha",
+src: "audo/020.mp3",}
+,{ name: " Al-Anbiya ",
+src: "audo/021.mp3",}
+,{ name: " Al-Hajj ",
+src: "audo/022.mp3",}
+,{ name: " Al-Mu'minun",
+src: "audo/023.mp3",}
+,{ name: "An-Nur ",
+src: "audo/024.mp3",}
+,{ name: " Al-Furqan ",
+src: "audo/025.mp3",}
+,{ name: " Ash-Shu'ara' ",
+src: "audo/026.mp3",}
+,{ name: "An-Naml ",
+src: "audo/027.mp3",}
+,{ name: "Al-Qasas ",
+src: "audo/028.mp3",}
+,{ name: " Al-'Ankabut ",
+src: "audo/029.mp3",}
+,{ name: "Ar­Room ",
+src: "audo/030.mp3",}
+,{ name: " Luqman ",
+src: "audo/031.mp3",}
+,{ name: " As­Sajdah ",
+src: "audo/032.mp3",}
+,{ name: " Al­Ahzab ",
+src: "audo/033.mp3",}
+,{ name: " Saba' ",
+src: "audo/034.mp3",}
+,{ name: " Fatir ",
+src: "audo/035.mp3",}
+,{ name: " Ya­Sin ",
+src: "audo/036.mp3",}
+,{ name: " as-Saffat ",
+src: "audo/037.mp3",}
+,{ name: " Sad ",
+src: "audo/038.mp3",}
+,{ name: " Az-Zumar ",
+src: "audo/039.mp3",}
+,{ name: "Ghafir ",
+src: "audo/040.mp3",}
+,{ name: " Fussilat ",
+src: "audo/041.mp3",}
+,{ name: " Ash-Shura ",
+src: "audo/042.mp3",}
+,{ name: " Az-Zukhruf ",
+src: "audo/043.mp3",}
+,{ name: "Ad-Dukhan ",
+src: "audo/044.mp3",}
+,{ name: " Al-Jathiya ",
+src: "audo/045.mp3",}
+,{ name: " Al-Ahqaf",
+src: "audo/046.mp3",}
+,{ name: "Muhammad ",
+src: "audo/047.mp3",}
+,{ name: "Al-Fath ",
+src: "audo/048.mp3",}
+,{ name: "Al-Hujurat ",
+src: "audo/049.mp3",}
+,{ name: "Qaf ",
+src: "audo/050.mp3",}
+,{ name: " Az-Zariyat ",
+src: "audo/051.mp3",}
+,{ name: " At-Tur",
+src: "audo/052.mp3",}
+,{ name: " An-Najm ",
+src: "audo/053.mp3",}
+,{ name: " Al-Qamar",
+src: "audo/054.mp3",}
+,{ name: "Ar-Rahman ",
+src: "audo/055.mp3",}
+,{ name: "Al-Waqi'ah ",
+src: "audo/056.mp3",}
+,{ name: "Al-Hadid ",
+src: "audo/057.mp3",}
+,{ name: "Al-Mujadilah ",
+src: "audo/058.mp3",}
+,{ name: "Al-Hashr",
+src: "audo/059.mp3",}
+,{ name: "Al-Mumtahinah",
+src: "audo/060.mp3",}
+,{ name: "As-Saff",
+src: "audo/061.mp3",}
+,{ name: "Al-Jumu'ah ",
+src: "audo/062.mp3",}
+,{ name: "Al-Munafiqun",
+src: "audo/063.mp3",}
+,{ name: "At-Taghabun",
+src: "audo/064.mp3",}
+,{ name: "At-Talaq ",
+src: "audo/065.mp3",}
+,{ name: "At-Tahrim",
+src: "audo/066.mp3",}
+,{ name: "Al-Mulk ",
+src: "audo/067.mp3",}
+,{ name: "Al-Qalam",
+src: "audo/068.mp3",}
+,{ name: "Al-Haqqah",
+src: "audo/069.mp3",}
+,{ name: "Al-Ma'arij",
+src: "audo/070.mp3",}
+,{ name: "Nooh",
+src: "audo/071.mp3",}
+,{ name: "Al-Jinn",
+src: "audo/072.mp3",}
+,{ name: "Al-Muzzammil",
+src: "audo/073.mp3",}
+,{ name: "Al-Muddaththir",
+src: "audo/074.mp3",}
+,{ name: "Al-Qiyamah",
+src: "audo/075.mp3",}
+,{ name: "Al-Insan",
+src: "audo/076.mp3",}
+,{ name: "Al-Mursalat ",
+src: "audo/077.mp3",}
+,{ name: "An-Naba'",
+src: "audo/078.mp3",}
+,{ name: "An-Nazi'at",
+src: "audo/079.mp3",}
+,{ name: " Abasa ",
+src: "audo/080.mp3",}
+,{ name: "At-Takwir",
+src: "audo/081.mp3",}
+,{ name: "Al-Infitar",
+src: "audo/082.mp3",}
+,{ name: "Al-Mutaffifin",
+src: "audo/083.mp3",}
+,{ name: "Al-Inshiqaq",
+src: "audo/084.mp3",}
+,{ name: "Al-Buruj",
+src: "audo/085.mp3",}
+,{ name: "At-Tariq",
+src: "audo/086.mp3",}
+,{ name: "Al-A'la ",
+src: "audo/087.mp3",}
+,{ name: " Al-Ghashiyah",
+src: "audo/088.mp3",}
+,{ name: "Al-Fajr",
+src: "audo/089.mp3",}
+,{ name: "Al-Balad ",
+src: "audo/090.mp3",}
+,{ name: "Ash-Shams ",
+src: "audo/091.mp3",}
+,{ name: "Al-Lail",
+src: "audo/092.mp3",}
+,{ name: "Ad-Duha",
+src: "audo/093.mp3",}
+,{ name: "Ash-Sharh",
+src: "audo/094.mp3",}
+,{ name: "At-Tin",
+src: "audo/095.mp3",}
+,{ name: "Al-'Alaq",
+src: "audo/096.mp3",}
+,{ name: " Al-Qadr ",
+src: "audo/097.mp3",}
+,{ name: "Al-Baiyinah ",
+src: "audo/098.mp3",}
+,{ name: " Az-Zalzalah",
+src: "audo/099.mp3",}
+,{ name: "Al-'Adiyat ",
+src: "audo/100.mp3",}
+,{ name: "Al-Qari'ah",
+src: "audo/101.mp3",}
+,{ name: "At-Takathur",
+src: "audo/102.mp3",}
+,{ name: "Al-'Asr",
+src: "audo/103.mp3",}
+,{ name: "Al-Humazah",
+src: "audo/104.mp3",}
+,{ name: "Al-Fil",
+src: "audo/105.mp3",}
+,{ name: "Quraish",
+src: "audo/106.mp3",}
+,{ name: "Al-Ma'un",
+src: "audo/107.mp3",}
+,{ name: "Al-Kauthar",
+src: "audo/108.mp3",}
+,{ name: "Al-Kafirun ",
+src: "audo/109.mp3",}
+,{ name: " An-Nasr",
+src: "audo/110.mp3",}
+,{ name: " Al-Masad ",
+src: "audo/111.mp3",}
+,{ name: "Al-Ikhlas ",
+src: "audo/112.mp3",}
+,{ name: "Al-Falaq",
+src: "audo/113.mp3",}
+,{ name: "An-Nas",
+src: "audo/114.mp3",}
+  ]
+
+
+  // this is the end of the valuble stuff
+  // we do same HTML here
+   let html = ""
+   adus.forEach((data)=>{
+    
+    html += `
+    <div class="namee">${data.name}</div>
+    <audio  calss="next"   controls="true">
+    <source src="${data.src}" type="audio/mp3">
+    </audio>
+   
+    `
+
+   })
+   document.querySelector("#con").innerHTML = html
+
+
+// we do the green top bar code here
+
+
+
+
+
+
+let green = document.querySelector(".top")
+green.addEventListener("click",()=>{
+    open("index.html","_self")
+})
+window.addEventListener("resize",()=>{
+
+  if (window.innerWidth > 500) {
+    document.querySelector(".donot").style.display = "block"
+  }else{
+    document.querySelector(".donot").style.display = "none"
+  }
+})
+if (window.innerWidth > 500) {
+  document.querySelector(".donot").style.display = "block"
+}else{
+  document.querySelector(".donot").style.display = "none"
+}
+
+
+
+
+// this json file does not do anything we just included if we need it in the future
+fetch("data.json").then((data)=>{
+  
+})
